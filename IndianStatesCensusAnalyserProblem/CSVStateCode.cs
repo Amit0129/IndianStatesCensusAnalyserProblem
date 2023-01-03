4df56f20e7444d3ff75f7888643648d759116ff4
Amit0129 <amitkumarnayak40@gmail.com>
2023-01-03 12:35:42 +0530

    [Amit] Reading data from IndianStateCode.csv file

---
+++ IndianStatesCensusAnalyserProblem/CSVStateCode.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyserProblem
+{
+    internal class CSVStateCode
+    {
+        public void ReadStateCodeData(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<StateCodeData>().ToList();
+                foreach (var data in records)
+                {
+                    Console.WriteLine($"SrialNo: {data.SrNo}\tState Name: {data.StateName}\tTIN: {data.TIN}\tStateCode: {data.StateCode}");
+                }
+            }
+        }
+    }
+}
--- IndianStatesCensusAnalyserProblem/Program.cs
+++ IndianStatesCensusAnalyserProblem/Program.cs
@@ -3,10 +3,15 @@
     internal class Program
     {
         public static string stateCensusCSVFilePath = @"D:\Brizlab\All.netprog\NewBatch\IndianStateCensusProblems\IndianStatesCensusAnalyserProblem\IndianStatesCensusAnalyserProblem\File\StateCensusData.csv";
+
+        public static string stateCodeCSVFilePath = @"D:\Brizlab\All.netprog\NewBatch\IndianStateCensusProblems\IndianStatesCensusAnalyserProblem\IndianStatesCensusAnalyserProblem\File\StateCode.csv";
         static void Main(string[] args)
         {
             CSVStateCensus cSVStateCensus = new CSVStateCensus();
-            cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath);
+            cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath); //Reading StateCensus CSV File
+
+            CSVStateCode cSVStateCode = new CSVStateCode();
+            cSVStateCode.ReadStateCodeData(stateCodeCSVFilePath); //Reading StateCode CSV File
         }
     }
 }
