7ccf5711f8afd6701d42bea8683baa45e79b6930
Amit0129 <amitkumarnayak40@gmail.com>
2023-01-03 11:55:48 +0530

    [Amit] Reading data from CSV file

---
+++ IndianStatesCensusAnalyserProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace IndianStatesCensusAnalyserProblem
+{
+    internal class Program
+    {
+        public static string stateCensusCSVFilePath = @"D:\Brizlab\All.netprog\NewBatch\IndianStateCensusProblems\IndianStatesCensusAnalyserProblem\IndianStatesCensusAnalyserProblem\File\StateCensusData.csv";
+        static void Main(string[] args)
+        {
+            CSVStateCensus cSVStateCensus = new CSVStateCensus();
+            cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath);
+        }
+    }
+}
